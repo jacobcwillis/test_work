2529b25c03a1de0bc4273155cb536a2dcaa986aa
Jake Willis <jake.willis@mrm.com>
2021-01-21 15:26:06 -0500

    Now an array of data is imported from JS to HTML

--- js/myApp.js
+++ js/myApp.js
@@ -1,7 +1,22 @@
 var myApp = angular.module('myApp', [])
 
 myApp.controller('myController', function myController($scope) {
-    $scope.items = {
-        "label": "Item1"
-    }
+    $scope.items = [
+        {
+            "label": "Header Item 1",
+            "url": ""
+        },
+        {
+            "label": "Header Item 2",
+            "url": ""
+        },
+        {
+            "label": "Header Item 3",
+            "url": ""
+        },
+        {
+            "label": "Header Item 4",
+            "url": ""
+        }
+    ]
 })
