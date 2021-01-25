823fc790995c9cbfcdd798ddb853808a80bebaf0
Edwards, Allan (DET-MRM) <allan.edwards@mrm.com>
2021-01-25 10:28:34 -0500

    additional updates

--- js/myApp.js
+++ js/myApp.js
@@ -1,1 +1,28 @@
 console.log("Hello World");
+
+var app = angular.module('testWork', ['ngRoute']);
+
+app.config(function () {
+	// The config function is fired once per page load and is used to define
+	// global app behaviour, it is most often used for managing Network events
+	// and global state / routing tools
+});
+
+
+
+app.controller('globalHelloWorldController', globalHelloWorldController);
+globalHelloWorldController.$inject = ['$scope', '$rootScope'];
+
+function globalHelloWorldController($scope, $rootScope) {
+	$rootScope.globalHelloName = 'Global Hello John Smith';
+}
+
+
+
+app.controller('localHelloWorldController', localHelloWorldController);
+localHelloWorldController.$inject = ['$scope', '$rootScope'];
+
+function localHelloWorldController($scope, $rootScope) {
+	//$rootScope.globalHelloName = '';
+  $scope.localHelloName = 'Local Hello John Smith';
+}
