9d2f94eef833b0315df2d6714293c9a7822f8cca
Jake Willis <jake.willis@mrm.com>
2021-02-03 16:39:30 -0500

    gitignore for node-modules, functioning gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,32 @@
-function defaultTask(cb) {
-    // place code for your default task here
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-    var gulp = require('gulp');
-    var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
-    sass.compiler = require('node-sass');
+// function defaultTask(cb) {
+//     // place code for your default task here
+//     return gulp.src('./scss/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./css'));
 
-    gulp.task('sass', function () {
-        return gulp.src('./scss/**/*.scss')
-            .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest('./css'));
-    });
+//     cb();
+// }
+// exports.default = defaultTask
 
-    gulp.task('sass:watch', function () {
-        gulp.watch('./scss/**/*.scss', ['sass']);
-    });
+gulp.task('appCSS', function (done) {
+    return gulp.src([
+        './scss/**/*.scss'
+    ])
+        //.pipe(concat('app.css'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+    done();
+});
 
 
 
+gulp.task('default', gulp.series((done) => {
+    // Watch SCSS
+    gulp.watch('./scss/**/*.scss', gulp.series(['appCSS']));
 
-
-    cb();
-}
-exports.default = defaultTask
-
+}));
