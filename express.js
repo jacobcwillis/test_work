463a9538822a885a33c3075dab386bffb597934a
Jake Willis <jake.willis@mrm.com>
2021-04-27 10:22:19 -0400

    index on localhost

--- express.js
+++ express.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-	response.send('Hello World');
+	response.send('../build/index.html');
 });
 
 app.get('/readdata', (request, response) => {
